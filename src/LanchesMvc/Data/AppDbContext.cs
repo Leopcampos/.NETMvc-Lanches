36ad5ba99e299761ab3ad6408d1b83f83eae7c8e
Leo Campos <leopcampos85@gmail.com>
2024-01-03 17:58:44 -0300

    Configurando o Identity no projeto.

--- src/LanchesMvc/Data/AppDbContext.cs
+++ src/LanchesMvc/Data/AppDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using LanchesMvc.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LanchesMvc.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<IdentityUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {}
