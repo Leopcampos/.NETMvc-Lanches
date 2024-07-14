4afaf7b05c004d90a52413bd12af406cd08ef16c
Leo Campos <leopcampos85@gmail.com>
2024-07-13 21:39:43 -0300

    Editando a classe

--- src/LanchesMvc/Repositories/CategoriaRepository.cs
+++ src/LanchesMvc/Repositories/CategoriaRepository.cs
@@ -2,17 +2,14 @@
 using LanchesMvc.Models;
 using LanchesMvc.Repositories.Interfaces;
 
-namespace LanchesMvc.Repositories
+namespace LanchesMvc.Repositories;
+
+public class CategoriaRepository : ICategoriaRepository
 {
-    public class CategoriaRepository : ICategoriaRepository
-    {
-        private readonly AppDbContext _context;
+    private readonly AppDbContext _context;
 
-        public CategoriaRepository(AppDbContext context)
-        {
-            _context = context;
-        }
+    public CategoriaRepository(AppDbContext context)
+        => _context = context;
 
-        public IEnumerable<Categoria> Categorias => _context.Categorias;
-    }
+    public IEnumerable<Categoria> Categorias => _context.Categorias;
 }
