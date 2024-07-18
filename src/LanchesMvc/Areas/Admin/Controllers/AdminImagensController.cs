f1e4b689fff4118138c15447aa444cb4292da771
Leo Campos <leopcampos85@gmail.com>
2024-07-17 21:01:31 -0300

    Implementação - Exibindo as opções para Upload e Consulta de imagens

---
+++ src/LanchesMvc/Areas/Admin/Controllers/AdminImagensController.cs
@@ -0,0 +1,25 @@
+﻿using LanchesMvc.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace LanchesMvc.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Authorize(Roles = "Admin")]
+public class AdminImagensController : Controller
+{
+    private readonly ConfigurationImagens _myConfig;
+    private readonly IWebHostEnvironment _hostingEnvironment;
+
+    public AdminImagensController(IWebHostEnvironment hostingEnvironment, IOptions<ConfigurationImagens> myConfig)
+    {
+        _myConfig = myConfig.Value;
+        _hostingEnvironment = hostingEnvironment;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
